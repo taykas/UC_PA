dfb9ac4e72ff7d1da62faf5dd01efdecc35ee9ee
tayka <thaynaschaeffer@gmail.com>
2025-08-28 10:31:15 -0300

    React

--- 07 React/Aula React/src/App.jsx
+++ 07 React/Aula React/src/App.jsx
@@ -1,19 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { YellowSquare } from './components/YellowSquare';
 import { RedSquare } from './components/RedSquare';
 import { BlueSquare } from './components/BlueSquare';
+import { Square } from './components/Square';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <YellowSquare />
-      <RedSquare />
-      <BlueSquare />
+      <Square classe="blue"/>
+      <Square classe="yellow"/>
+      <Square classe="red"/>
     </>
   )
 }
---
+++ 07 React/Aula React/src/components/Square.jsx
@@ -0,0 +1,7 @@
+import './Square.css'
+
+export const Square = ({ classe }) => {
+    return (
+        <div className={`square ${classe}`}></div>
+    )
+}
