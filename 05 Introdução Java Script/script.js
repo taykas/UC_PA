8d61b090781939adacc8c6bc6b61168d0fc9f04e
taykas <thaynaschaeffer@gmail.com>
2025-09-03 20:29:46 -0300

    esqueci o que eu fiz aqui

--- 05 Introdução Java Script/script.js
+++ 05 Introdução Java Script/script.js
@@ -1,17 +1,17 @@
 let changeColor = () => {
-    let squareColor = document.getElementById('input').value
-    console.log(squareColor)
+    // let squareColor = document.getElementById('input').value
+    // console.log(squareColor)
 
-    let square = document.getElementById('square')
+    // let square = document.getElementById('square')
 
-    square.style.backgroundColor = squareColor
+    // square.style.backgroundColor = squareColor
 
-    // let square = document.getElementById("square");
-    // if (square.classList.contains("ok")) {
-    //     square.classList.remove("ok")
-    //     square.classList.add("bad")
-    // } else {
-    //     square.classList.remove("bad")
-    //     square.classList.add("ok")
-    // }
+    let square = document.getElementById("square");
+    if (square.classList.contains("ok")) {
+        square.classList.remove("ok")
+        square.classList.add("bad")
+    } else {
+        square.classList.remove("bad")
+        square.classList.add("ok")
+    }
 }
