ed3e6f256aa995431e79a202b721fe3457aaf7a2
taykas <thaynaschaeffer@gmail.com>
2025-09-03 11:46:37 -0300

    bah

--- 09 React/aula9react/src/App.jsx
+++ 09 React/aula9react/src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter } from "react-router-dom"
+import { Home } from "./components/Home"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [num, setNum] = useState(0)
+
+  useEffect(() => {
+    console.log("Quero ir pra casa")
+  }, [num])
 
   return (
     <>
-      
+      {/* <button onClick={ () => setNum(num => num + 1)}>Incrementar</button>
+      <h1>{num}</h1> */}
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/Home' element={<Home/>}></Route>
+          <Route path='/sobre' element={<Sobre/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
