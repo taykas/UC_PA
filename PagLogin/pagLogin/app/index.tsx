7c560472833fcbe9953d77377ce0c81fa9bf86e2
taykas <thaynaschaeffer@gmail.com>
2025-11-12 11:12:08 -0300

    Sing UP Page

--- PagLogin/pagLogin/app/index.tsx
+++ PagLogin/pagLogin/app/index.tsx
@@ -1,5 +1,4 @@
-import { Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import { rgbaColor } from 'react-native-reanimated/lib/typescript/Colors';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 const styles = StyleSheet.create ({
   main: {
@@ -28,7 +27,7 @@ const styles = StyleSheet.create ({
   },
   button_container: {
     flexDirection: 'row',
-    gap: 100
+    gap: 70
   },
   sing_up_container: {
     flexDirection: 'row',
@@ -39,24 +38,31 @@ const styles = StyleSheet.create ({
 export default function HomeScreen() {
   return (
     <>
-    <View style={styles.main}>
-      <View style={styles.container}>
-          <Text style={{alignSelf: 'flex-start', fontSize: 20, marginBottom: 10}}>Wellcome!</Text>
-          <TextInput style={styles.input}
-            placeholder='Email'></TextInput>
+      <View style={styles.main}>
+        <View style={styles.container}>
+          <Text style={{alignSelf: 'flex-start', fontSize: 20, marginBottom: 10}}>Create Account!</Text>
 
           <TextInput style={styles.input}
-            placeholder='Password'></TextInput>
+          placeholder='Name'></TextInput>
+
+          <TextInput style={styles.input}
+          placeholder='Name'></TextInput>
+
+          <TextInput style={styles.input}
+          placeholder='Email'></TextInput>
+
+          <TextInput style={styles.input}
+          placeholder='Password' secureTextEntry={true}></TextInput>
 
           <TouchableOpacity style={styles.login_bottom}>
             <View>
-              <Text style={{color: 'white', textAlign: 'center'}}>Login</Text>
+              <Text style={{color: 'white', textAlign: 'center'}}>Create</Text>
             </View>
           </TouchableOpacity>
 
           <TouchableOpacity style={{marginTop: 20, marginBottom: 20}}>
             <View>
-              <Text style={{color: '#6e4ce6'}}>Forgot Password?</Text>
+              <Text style={{color: '#6e4ce6'}}>Access Quickly</Text>
             </View>
           </TouchableOpacity>
 
@@ -67,24 +73,109 @@ export default function HomeScreen() {
               </View>
             </TouchableOpacity>
 
-            <TouchableOpacity>
-              <View>
-                <Text style={{fontSize: 15, marginBottom: 20, color: '#0615b1'}}>Facebook</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
+          <TouchableOpacity>
+            <View>
+              <Text style={{fontSize: 15, marginBottom: 20, color: '#0615b1'}}>Facebook</Text>
+            </View>
+          </TouchableOpacity>
 
-          <View style={styles.sing_up_container}>
-            <Text>Don't have an account?</Text>
-            <TouchableOpacity>
-              <View>
-                <Text style={{color: '#7067cb'}}>Sing Up</Text>
-              </View>
-            </TouchableOpacity>
+          <TouchableOpacity>
+            <View>
+              <Text style={{fontSize: 15, marginBottom: 20, color: '#5d24c1'}}>Linkedin</Text>
+            </View>
+          </TouchableOpacity>
           </View>
         </View>
       </View>
     </>
-  )
-}
+    )
+};
+
+
+// const styles = StyleSheet.create ({
+//   main: {
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     flex: 1
+//   },
+//   container: {
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     flex: 1
+//   },
+//   input: {
+//     padding: 9,
+//     color: '#a3a3a3',
+//     backgroundColor: '#ededed',
+//     marginBottom: 10,
+//     borderRadius: 10,
+//     width: 300
+//   },
+//   login_bottom: {
+//     width: 300,
+//     padding: 10,
+//     backgroundColor: '#a385d1',
+//     borderRadius: 10
+//   },
+//   button_container: {
+//     flexDirection: 'row',
+//     gap: 70
+//   },
+//   sing_up_container: {
+//     flexDirection: 'row',
+//     gap: 5
+//   }
+// });
+
+// export default function HomeScreen() {
+//   return (
+//     <>
+//     <View style={styles.main}>
+//       <View style={styles.container}>
+//           <Text style={{alignSelf: 'flex-start', fontSize: 20, marginBottom: 10}}>Wellcome!</Text>
+//           <TextInput style={styles.input}
+//             placeholder='Email'></TextInput>
+
+//           <TextInput style={styles.input}
+//             placeholder='Password' secureTextEntry={true}></TextInput>
+
+//           <TouchableOpacity style={styles.login_bottom}>
+//             <View>
+//               <Text style={{color: 'white', textAlign: 'center'}}>Login</Text>
+//             </View>
+//           </TouchableOpacity>
+
+//           <TouchableOpacity style={{marginTop: 20, marginBottom: 20}}>
+//             <View>
+//               <Text style={{color: '#6e4ce6'}}>Forgot Password?</Text>
+//             </View>
+//           </TouchableOpacity>
+
+//           <View style={styles.button_container}>
+//             <TouchableOpacity>
+//               <View>
+//                 <Text style={{fontSize: 15, marginBottom: 20, color: '#5d24c1'}}>Google</Text>
+//               </View>
+//             </TouchableOpacity>
+
+//             <TouchableOpacity>
+//               <View>
+//                 <Text style={{fontSize: 15, marginBottom: 20, color: '#0615b1'}}>Facebook</Text>
+//               </View>
+//             </TouchableOpacity>
+//           </View>
+
+//           <View style={styles.sing_up_container}>
+//             <Text style={{color: '#a18bef'}}>Don't have an account?</Text>
+//             <TouchableOpacity>
+//               <View>
+//                 <Text style={{color: '#7067cb'}}>Sing Up</Text>
+//               </View>
+//             </TouchableOpacity>
+//           </View>
+//         </View>
+//       </View>
+//     </>
+//   )
+// }
 
---
+++ PagLogin/pagLogin/app/singUp.tsx
@@ -0,0 +1,6 @@
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+export default function SingUpScreen() {
+  return (
+    <></>
+  )}
